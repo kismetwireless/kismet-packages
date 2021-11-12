dfb5f0c2fcaab349f695930154c4261a059751d3
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2021-11-12 11:40:48 -0500

    Add bullseye arm, impish release, single-fire git script

--- scripts/build_and_update.sh
+++ scripts/build_and_update.sh
@@ -5,6 +5,6 @@ SRC_DIR=${SRC_DIR:-$2}
 export BASE_DIR
 export SRC_DIR
 
-${BASE_DIR}/scripts/rebuild_all_git.sh
+${BASE_DIR}/scripts/rebuild_all_git.sh $*
 ${BASE_DIR}/scripts/aptly_update_repos.sh
 
