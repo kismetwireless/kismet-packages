447902ec9ef7b0530ea0b1c3f6e0d42ea6d546a2
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2022-01-22 16:18:38 -0500

    Start adding hirsute, fix focal migration to debootstrap component list

---
+++ rootfs/scripts/launch/ubuntu_hirsute.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+if [ "${VERSION}"x != "x" ]; then
+    ln -s /scripts/fpm-${VERSION} /tmp/fpm
+    ln -s /scripts/build-${VERSION} /tmp/build
+else
+    ln -s /scripts/fpm-master /tmp/fpm
+    ln -s /scripts/build-master /tmp/build
+fi
+
+exec /tmp/build/build_ubuntu_hirsute.sh
