fa4ab9addfa8b19b7f16a613ffcc344b6a68e098
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2022-01-27 13:24:48 -0500

    Snapshot 2022-01-R3 scripts

---
+++ scripts/build-2022-01-R3/build_debian_bullseye_cross.sh
@@ -0,0 +1,43 @@
+#!/bin/sh -e
+
+cd /build
+
+git clone --recursive /kismet
+cd kismet
+git checkout master
+
+if [ "${CHECKOUT}"x != "x" ]; then
+	echo "Checking out ${CHECKOUT}"
+	git checkout ${CHECKOUT}
+fi
+
+export PKG_CONFIG_DIR=""
+export PKG_CONFIG_PATH="/sysroot/usr/lib/pkgconfig:/sysroot/usr/share/pkgconfig:/sysroot/usr/lib/${ABI}/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="/sysroot"
+
+export AR=${ABI}-ar
+export CC=${ABI}-gcc
+export CXX=${ABI}-g++ 
+
+./configure \
+    --build=x86_64-linux \
+    --host=${ABI} \
+    CFLAGS="-I/sysroot/usr/include -I/sysroot/usr/include/${ABI}" \
+    CPPFLAGS="-I/sysroot/usr/include -I/sysroot/usr/include/${ABI}" \
+    CXXFLAGS="-I/sysroot/usr/include -I/sysroot/usr/include/${ABI}" \
+    LDFLAGS="-L/sysroot/usr/lib/${ABI} --sysroot=/sysroot" \
+    --prefix=/usr \
+    --sysconfdir=/etc/kismet \
+    --disable-element-typesafety 
+
+
+if [ "${NCORES}" = "" ]; then 
+	NCORES=$(($(nproc) / 2))
+fi
+make -j${NCORES}
+
+/tmp/fpm/fpm_debian_bullseye.sh
+/tmp/fpm/fpm_noarch_python3_deb.sh
+
+mv -v *.deb /dpkgs
+
---
+++ scripts/build-2022-01-R3/build_debian_buster.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -e
+
+cd /build
+
+git clone --recursive /kismet
+cd kismet
+git checkout master
+
+if [ "${CHECKOUT}"x != "x" ]; then
+	echo "Checking out ${CHECKOUT}"
+	git checkout ${CHECKOUT}
+fi
+
+./configure \
+    --prefix=/usr \
+    --sysconfdir=/etc/kismet \
+    --disable-element-typesafety \
+    --disable-libwebsockets 
+
+if [ "${NCORES}" = "" ]; then 
+	NCORES=$(($(nproc) / 2))
+fi
+make -j${NCORES}
+
+/tmp/fpm/fpm_debian_buster.sh
+/tmp/fpm/fpm_noarch_python3_deb.sh
+
+mv -v *.deb /dpkgs
+
