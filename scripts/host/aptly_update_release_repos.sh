b649ceaaf87db2d6c1275bad9e8c9b22cf7b901d
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2022-04-04 11:42:29 -0400

    Builds: Add jammy to aptly updates

--- scripts/host/aptly_update_release_repos.sh
+++ scripts/host/aptly_update_release_repos.sh
@@ -11,7 +11,7 @@ export gpg_tty=$(tty)
 
 export RELEASEMATCH=$2
 
-for dist in kali bionic buster bullseye disco focal hirsute impish kali xenial; do
+for dist in kali bionic buster bullseye disco focal hirsute impish kali xenial jammy; do
     ( 
         cd ${BASE_DIR}
         echo "Working on ${dist}"
