1329e7f483d649005883ae0a9c611decd9f4bfd0
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2022-08-07 17:00:54 -0400

    Add 202208R1 components

---
+++ scripts/build-2022-08-R1/build_ubuntu_hirsute.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -e
+
+cd /build
+
+git clone --recursive /kismet
+cd kismet
+git checkout master
+
+if [ "${CHECKOUT}"x != "x" ]; then
+	echo "Checking out ${CHECKOUT}"
+	git checkout ${CHECKOUT}
+fi
+
+
+./configure \
+    --prefix=/usr \
+    --sysconfdir=/etc/kismet \
+    --disable-element-typesafety 
+
+
+if [ "${NCORES}" = "" ]; then 
+	NCORES=$(nproc)
+fi
+make -j${NCORES}
+
+/tmp/fpm/fpm_ubuntu_hirsute.sh
+/tmp/fpm/fpm_noarch_python3_deb.sh
+
+mv -v *.deb /dpkgs
+
---
+++ scripts/build-2022-08-R1/build_ubuntu_xenial.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+cd /build
+
+git clone --recursive /kismet
+cd kismet
+git checkout master
+
+if [ "${CHECKOUT}"x != "x" ]; then
+	echo "Checking out ${CHECKOUT}"
+	git checkout ${CHECKOUT}
+fi
+
+
+./configure \
+    --prefix=/usr \
+    --sysconfdir=/etc/kismet \
+    --disable-element-typesafety \
+    --disable-python-tools \
+    --disable-libwebsockets 
+
+
+if [ "${NCORES}" = "" ]; then 
+	NCORES=$(nproc)
+fi
+make -j${NCORES}
+
+/tmp/fpm/fpm_ubuntu_xenial.sh
+
+mv -v *.deb /dpkgs
+
