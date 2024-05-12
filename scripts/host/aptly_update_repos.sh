9ede144d46d225f93077ebfb9821a118e4743612
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2024-05-12 12:10:52 -0400

    noble: Add builds for ubuntu-noble

--- scripts/host/aptly_update_repos.sh
+++ scripts/host/aptly_update_repos.sh
@@ -7,7 +7,7 @@ fi
 
 BASE_DIR=$1
 
-for dist in kali bionic bullseye bookworm focal disco xenial jammy kinetic lunar mantic; do
+for dist in kali bionic bullseye bookworm focal disco xenial jammy kinetic lunar mantic noble; do
 	( 
     	cd ${BASE_DIR}
         echo "Working on ${dist}"
