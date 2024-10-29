719ae43b9fc975a967b0e9b2176a1b338d2d9123
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2024-10-29 09:42:01 -0400

    oracular: Add ubuntu oracular builds

---
+++ scripts/build-master/build_ubuntu_oracular.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -e
+
+cd /build
+
+git config --global --add safe.directory /kismet/.git
+git clone --recursive /kismet
+cd kismet
+git checkout master
+
+if [ "${CHECKOUT}"x != "x" ]; then
+	echo "Checking out ${CHECKOUT}"
+	git checkout ${CHECKOUT}
+fi
+
+./configure \
+    --prefix=/usr \
+    --sysconfdir=/etc/kismet \
+    --disable-element-typesafety \
+    --enable-bladerf \
+    --enable-wifi-coconut
+
+
+if [ "${NCORES}" = "" ]; then 
+	NCORES=$(nproc)
+fi
+make -j${NCORES}
+
+/tmp/fpm/fpm_ubuntu_oracular.sh
+
+mv -v *.deb /dpkgs
+
+# Build wifi coconut 
+
+cd /build 
+git config --global --add safe.directory /build/hak5-wifi-coconut/.git
+git clone https://github.com/hak5/hak5-wifi-coconut
+cd hak5-wifi-coconut 
+mkdir build 
+cd build 
+cmake ../ -DCMAKE_INSTALL_PREFIX=/usr
+make -j${NCORES} 
+/tmp/fpm/fpm_ubuntu_oracular_coconut.sh
+
+mv -v *.deb /dpkgs
+
